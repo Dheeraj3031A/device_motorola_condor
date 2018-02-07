7aafa73ac0ee4355c977438ad43e670c289a794c
Bruno Martins <bgcngm@gmail.com>
2018-02-07 22:47:01 +0000

    condor: Move fstab to /vendor
    Change-Id: Ie1c6cb662f2f54c7c14d3e7d55aedbfbeef5f8a7

---
+++ ramdisk/Android.mk
@@ -0,0 +1,10 @@
+LOCAL_PATH:= $(call my-dir)
+
+# Fstab
+include $(CLEAR_VARS)
+LOCAL_MODULE       := fstab.qcom
+LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := fstab.qcom
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)
+include $(BUILD_PREBUILT)
