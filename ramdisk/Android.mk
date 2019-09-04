08fc67061d61c9e94df400d996f732b39070ed9a
Alessandro Astone <ales.astone@gmail.com>
2019-09-04 18:18:15 +0200

    condor: address LOCAL_MODULE_TAGS := eng is obsolete
     * We're always including the modules we want built, so we can
       safely remove the eng tags
    
    Change-Id: I41c98eeaf68f701bb8a6460b987f6f1743c92df3

--- ramdisk/Android.mk
+++ ramdisk/Android.mk
@@ -3,8 +3,16 @@ LOCAL_PATH:= $(call my-dir)
 # Fstab
 include $(CLEAR_VARS)
 LOCAL_MODULE       := fstab.qcom
-LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := fstab.qcom
 LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := init.device.rc
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := init.device.rc
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init/hw
 include $(BUILD_PREBUILT)
