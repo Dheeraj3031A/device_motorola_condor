d84f034a1548f61f6d978f7ddb7bf63e5b8e6f4b
jhenrique09 <jhenrique09.mcz@hotmail.com>
2019-09-16 19:02:36 +0000

    condor: Correct define libinit variable
    Fixes:
    
    15:55:35 error: system/core/init/Android.bp:95:1: "libinit" depends on undefined module "//device/oneplus/oneplus3/init/libinit_oneplus3"
    
    Change-Id: Ia1a58bea3df625fd76c060370dc7f9fa2a597765

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -38,7 +38,7 @@ TARGET_RECOVERY_FSTAB := $(LOCAL_PATH)/ramdisk/fstab.qcom
 TARGET_OTA_ASSERT_DEVICE := xt1021,xt1022,xt1023,condor_umts,condor_umtsds,condor
 
 # Init
-TARGET_INIT_VENDOR_LIB := libinit_condor
+TARGET_INIT_VENDOR_LIB := //$(LOCAL_PATH):libinit_condor
 TARGET_RECOVERY_DEVICE_MODULES := libinit_condor
 
 #BOARD_SEPOLICY_DIRS += \
