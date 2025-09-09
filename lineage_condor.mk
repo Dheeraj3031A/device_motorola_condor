c2edd115fc588bba1a285c6f876a708ed7471b43
Dheeraj R Shrivatsa <Dheeraj3031A@users.noreply.github.com>
2025-09-09 18:27:30 +0530

    condor: Unify full_condor and lineage_condor

--- lineage_condor.mk
+++ lineage_condor.mk
@@ -1,11 +1,11 @@
 #
-# Copyright (C) 2014 Prashant Gahlot (proxthehacker@gmail.com)
+# Copyright (C) 2014 The CyanogenMod Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-#      http://www.apache.org/licenses/LICENSE-2.0
+# http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
@@ -14,17 +14,20 @@
 # limitations under the License.
 #
 
-# Sample: This is where we'd set a backup provider if we had one
-# $(call inherit-product, device/sample/products/backup_overlay.mk)
+# Inherit from condor device
+$(call inherit-product, device/motorola/condor/device.mk)
+
+# Inherit some common Lineage stuff.
+$(call inherit-product, vendor/lineage/config/common_mini_phone.mk)
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit from condor device
-$(call inherit-product, device/motorola/condor/device.mk)
-
-PRODUCT_NAME := full_condor
+## Device identifier. This must come after all inclusions
+PRODUCT_NAME := lineage_condor
+PRODUCT_MODEL := condor
 PRODUCT_DEVICE := condor
 PRODUCT_BRAND := motorola
-PRODUCT_MODEL := condor
 PRODUCT_MANUFACTURER := motorola
+
+PRODUCT_GMS_CLIENTID_BASE := android-motorola
